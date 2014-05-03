eafafa713b639232cf546d5a83523d3c7c822dd1
Prafulla <prafulla@mit.edu>
2014-05-03 17:57:56 -0400

    Added server-client

---
+++ src/client/PingballClient.java
@@ -0,0 +1,232 @@
+package client;
+
+import java.io.*;
+import java.net.*;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Queue;
+import java.util.LinkedList;
+import java.util.NoSuchElementException;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+
+import physics.Board;
+
+/**
+ * Client for a Pingball simulator.
+ *
+ * This client supports operation in single player mode and multiplayer
+ * mode, where it can connect to a server.
+ *
+ * Thread safety argument:
+ *
+ * PingballClient will have a main render thread, and two background threads
+ * for sending and receiving messages from the server. Messages can include
+ * adding/removing of balls, adding/removing transparent walls, etc.
+ *
+ * Each of the background threads will have a blocking queue shared with the
+ * main render thread, for sending messages to and from the server.
+ *
+ * The main render thread, at every frame render, will poll to see if there
+ * are any messages available from the background thread and perform the
+ * corresponding actions. When there are messages to send to the server,
+ * the messages will be put in the queue for the thread responsible for sending
+ * messages to the server, and this background thread will perform the actual
+ * sending operation.
+ */
+public class PingballClient {
+
+    private static final int DEFAULT_PORT = 10987;
+
+    private static final double FRAMERATE = 20; // in FPS
+
+    /**
+     * Start a PingballClient using the given arguments.
+     *
+     * Usage: PingballClient [--port PORT] [--host HOST] FILE
+     *
+     * The optional [--port PORT] argument is the port to connect to on the server.
+     *
+     * The optional [--host HOST] argument is the host to connect to.
+     *
+     * The FILE argument is the board file to read from.
+     */
+    public static void main(String[] args) throws IOException {
+        File file = null;
+        String host = null;
+        Integer port = DEFAULT_PORT;
+
+        Queue<String> arguments = new LinkedList<String>(Arrays.asList(args));
+        try {
+            while (!arguments.isEmpty()) {
+                String flag = arguments.remove();
+                try {
+                    if (flag.equals("--port")) {
+                        port = Integer.parseInt(arguments.remove());
+                        if (port < 0 || port > 65535) {
+                            throw new IllegalArgumentException("port " + port + " out of range");
+                        }
+                    } else if (flag.equals("--host")) {
+                        host = arguments.remove();
+                    } else if (file == null) {
+                        file = new File(flag);
+                        if (!file.isFile()) {
+                            throw new IllegalArgumentException("file not found: \"" + file + "\"");
+                        }
+                    } else {
+                        throw new IllegalArgumentException("unknown option: \"" + flag + "\"");
+                    }
+                } catch (NoSuchElementException nsee) {
+                    throw new IllegalArgumentException("missing argument for " + flag);
+                } catch (NumberFormatException nfe) {
+                    throw new IllegalArgumentException("unable to parse number for " + flag);
+                }
+            }
+            if (file == null) {
+                throw new IllegalArgumentException("missing positional argument FILE");
+            }
+        } catch (IllegalArgumentException iae) {
+            System.err.println(iae.getMessage());
+            System.err.println("usage: PingballClient [--host HOST] [--port PORT] FILE");
+            return;
+        }
+
+        final BlockingQueue<String> sendQueue = new LinkedBlockingQueue<>();
+        final BlockingQueue<String> receiveQueue = new LinkedBlockingQueue<>();
+        Board board = new Board(sendQueue, file);
+
+        if (host != null) {
+            try (Socket socket = new Socket(host, port)) {
+                Thread receiver = new Thread(new Receiver(socket, receiveQueue));
+                receiver.start();
+                Thread sender = new Thread(new Sender(socket, sendQueue));
+                sender.start();
+                mainLoop(board, receiveQueue);
+                receiver.join();
+                sender.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        } else {
+            mainLoop(board, receiveQueue);
+        }
+    }
+
+    /**
+     * Evolve the board for a frame.
+     */
+    private static void evolveFrame(Board board, BlockingQueue<String> receiveQueue) {
+        List<String> messages = new ArrayList<String>();
+        receiveQueue.drainTo(messages);
+        for (String message: messages) {
+            board.processMessage(message);
+        }
+        board.evolve(1.0 / FRAMERATE);
+    }
+
+    /**
+     * Main render loop of the client.
+     *
+     * This method evolves and renders the board. It tries to achieve a frame
+     * rate as close to FRAMERATE as possible.
+     */
+    private static void mainLoop(Board board, BlockingQueue<String> receiveQueue) {
+        while (true) {
+            long start = System.nanoTime();
+            evolveFrame(board, receiveQueue);
+            List<String> representation = board.gridRepresentation();
+            for (String line: representation) {
+                System.out.println(line);
+            }
+            long mid = System.nanoTime();
+            try {
+                long sleepTime = (long) (1e3 / FRAMERATE - (mid - start) / 1e6);
+                if (sleepTime > 0) {
+                    Thread.sleep(sleepTime);
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            long end = System.nanoTime();
+            
+            // compute stats
+            double frameRender = ((double) (mid - start) / 1e6);
+            double closeness = frameRender * FRAMERATE / 1e3 * 1e2; // percent
+            double fps = 1 / ((double) (end - start) / 1e9);
+            System.out.printf("%.1f FPS | %.1f msec render (%02.0f%%)%n", fps, frameRender, closeness);
+        }
+    }
+
+    /**
+     * A receiver to asynchronously receive data from a socket.
+     */
+    private static class Receiver implements Runnable {
+
+        private final Socket socket;
+        private final BlockingQueue<String> receiveQueue;
+
+        /**
+         * Make a Receiver.
+         *
+         * @param socket The socket to receive from.
+         *
+         * @param receiveQueue The queue to put received data into.
+         */
+        public Receiver(Socket socket, BlockingQueue<String> receiveQueue) throws IOException {
+            this.socket = socket;
+            this.receiveQueue = receiveQueue;
+        }
+
+        /**
+         * Receive data from the socket and put it into the queue.
+         */
+        public void run() {
+            try (BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()))){
+                for (String line = input.readLine(); line != null; line = input.readLine()) {
+                    receiveQueue.put(line);
+                }
+            } catch (InterruptedException | IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+    /**
+     * A sender to asynchronously send data to a socket.
+     */
+    private static class Sender implements Runnable {
+
+        private final Socket socket;
+        private final BlockingQueue<String> sendQueue;
+
+        /**
+         * Make a Sender.
+         *
+         * @param socket The socket to receive from.
+         *
+         * @param sendQueue The queue to put received data into.
+         */
+        public Sender(Socket socket, BlockingQueue<String> sendQueue) throws IOException {
+            this.socket = socket;
+            this.sendQueue = sendQueue;
+        }
+
+        /**
+         * Send data from the socket from the queue.
+         */
+        public void run() {
+            try (PrintWriter output = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()), true)) {
+                while (true) {
+                    String line = sendQueue.take();
+                    output.println(line);
+                }
+            } catch (InterruptedException | IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+}
---
+++ src/server/Router.java
@@ -0,0 +1,239 @@
+package server;
+
+import java.net.Socket;
+import java.io.PrintWriter;
+import java.io.IOException;
+import java.util.Set;
+import java.util.HashSet;
+import java.util.HashMap;
+import java.util.Map;
+
+import pingball.simulation.Wall;
+import util.BidirectionalMap;
+import util.Pair;
+import util.StringUtils;
+
+/** A thread-safe mutable routing to keep track of connections between walls.
+ *
+ * Sockets passed into this class must not be used for outputting data anywhere
+ * else. Clients of this class are allowed to read from sockets that are passed
+ * in.
+ *
+ * Representation invariant:
+ *
+ * The router keeps links of walls of boards to other walls. In order for the
+ * system to function correctly (and have valid meaning), these links must be
+ * bidirectional. The links in wallLinkMap have to be bidirectional at all
+ * times. If a link such as (a, left) -> b is in the wallLinkMap, then the
+ * opposite link (b, right) -> a must be present in the map.
+ *
+ * Thread safety argument:
+ *
+ * This class is meant to be shared between threads. All methods on router are
+ * synchronized, which will guarantee thread safety.
+ */
+public class Router {
+    
+    private final BidirectionalMap<Socket, String> mapSocketName = new BidirectionalMap<>();
+    
+    // store bidirectional links in here like:
+    // (a, left) -> b
+    // (b, right) -> a
+    private final Map<Pair<String, Wall.Side>, String> wallLinkMap = new HashMap<>();
+    
+    /**
+     * Make a Router.
+     */
+    public Router() {
+        //Do nothing
+    }
+    
+    /**
+     * Removes a client from the routing system.
+     *
+     * @param caller The user to remove.
+     *
+     * This method must be called when a user is removed from the system.
+     */
+    public synchronized void removeUser(Socket caller) {
+        if( mapSocketName.containsForward(caller) ) {
+            String user = mapSocketName.getForward(caller);
+            
+            for (Wall.Side wall: Wall.Side.values()) {
+                removeWallLinkOf(user, wall);
+            }
+            
+            mapSocketName.removeForward(caller);
+        }
+        checkRep();
+    }
+
+    public synchronized void send(Socket socket, String message) {
+        try {
+            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+            out.println(message);
+        } catch (IOException e) {
+            // ignore
+        }
+    }
+    
+    public synchronized Set<String> getUsers() {
+        checkRep();
+        return new HashSet<>(mapSocketName.valueSet());
+    }
+
+    /**
+     * Processes a message from the client.
+     *
+     * @param message The message to process.
+     *
+     * @param caller The recipient of the message.
+     */
+    public synchronized void processClientMessage(String message, Socket caller) {
+        String[] split = message.split(" ");
+
+        String hellomsg = "^hello [A-Za-z_][A-Za-z_0-9]*$";
+        String ballmsg = "^ball [A-Za-z_][A-Za-z_0-9]* (left|right|top|bottom)( -?(?:[0-9]+\\.[0-9]*|\\.?[0-9]+)){4}$";
+
+        if (message.matches(hellomsg)) {
+            addUser(split[1], caller);
+        } else if (message.matches(ballmsg)) {
+            String clientName = mapSocketName.getForward(caller);
+            Wall.Side wall = Wall.Side.fromString(split[2]);
+            Socket dest = mapSocketName.getReverse(wallLinkMap.get(Pair.of(clientName, wall)));
+            if (dest != null) {
+                split[2] = wall.opposite().toString();
+                send(dest, StringUtils.join(" ", split));
+            }
+        } else {
+            System.err.println("ignoring invalid message from client");
+        }
+        checkRep();
+    }
+
+    /**
+     * Links two boards together horizontally.
+     *
+     * This breaks any preexisting connections if necessary.
+     *
+     * @param left The left board to link.
+     *
+     * @param right The right board to link.
+     *
+     * @return Whether the boards were successfully linked.
+     */
+    public synchronized boolean addConnectionHorizontal(String left, String right) {
+        if (mapSocketName.getReverse(left) == null || mapSocketName.getReverse(right) == null) {
+            checkRep();
+            return false;
+        }
+        removeWallLinkOf(left, Wall.Side.RIGHT);
+        removeWallLinkOf(right, Wall.Side.LEFT);
+        
+        wallLinkMap.put(Pair.of(left, Wall.Side.RIGHT), right);
+        sendMessageMergeWall(mapSocketName.getReverse(left), Wall.Side.RIGHT, right);
+        
+        wallLinkMap.put(Pair.of(right, Wall.Side.LEFT), left);
+        sendMessageMergeWall(mapSocketName.getReverse(right), Wall.Side.LEFT, left);
+        checkRep();
+        return true;
+    }
+
+    /**
+     * Links two boards together vertically.
+     *
+     * This breaks any preexisting connections if necessary.
+     *
+     * @param top The top board to link.
+     *
+     * @param bottom The bottom board to link.
+     *
+     * @return Whether the boards were successfully linked.
+     */
+    public synchronized boolean addConnectionVertical(String top, String bottom) {
+        if (mapSocketName.getReverse(top) == null || mapSocketName.getReverse(bottom) == null) {
+            checkRep();
+            return false;
+        }
+        removeWallLinkOf(top, Wall.Side.BOTTOM);
+        removeWallLinkOf(bottom, Wall.Side.TOP);
+        
+        wallLinkMap.put(Pair.of(top, Wall.Side.BOTTOM), bottom);
+        sendMessageMergeWall(mapSocketName.getReverse(top), Wall.Side.BOTTOM, bottom);
+        
+        wallLinkMap.put(Pair.of(bottom, Wall.Side.TOP), top);
+        sendMessageMergeWall(mapSocketName.getReverse(bottom), Wall.Side.TOP, top);
+        checkRep();
+        return true;
+    }
+    
+    /**
+     * Remove the merging wall of that user on the corresonding side
+     * 
+     * @param user the name of user
+     * 
+     * @param side the side of the wall
+     */
+    private synchronized void removeWallLinkOf(String user, Wall.Side side) {
+        if( wallLinkMap.containsKey(Pair.of(user, side)) ) {
+            String anotherUser = wallLinkMap.get(Pair.of(user, side));
+            
+            wallLinkMap.remove(Pair.of(user, side));
+            sendMessageReturnToWall(mapSocketName.getReverse(user), side);
+            
+            removeWallLinkOf(anotherUser, side.opposite());
+        }
+    }
+    
+    /**
+     * Adds a client to the routing system.
+     *
+     * @param name The name of the board.
+     *
+     * @param socket The socket corresponding to the client.
+     */
+    private synchronized void addUser(String name, Socket socket) {
+        if( !mapSocketName.containsReverse(name) ) {
+            mapSocketName.putForward(socket, name);
+        }
+        checkRep();
+    }
+    
+    /**
+     * Send message to the user to make one side of his wall to wall (not a user name anymore)
+     * 
+     * @param socket The socket of client
+     * 
+     * @param side The side of the wall
+     */
+    private synchronized void sendMessageReturnToWall(Socket socket, Wall.Side side) {
+        send(socket, String.format("disconnect %s", side.toString()));
+    }
+    
+    /**
+     * Send message to the user to make one side of his wall merge with the corresponding name
+     * 
+     * @param socket The socket of client
+     * 
+     * @param side The side of the wall
+     * 
+     * @param name The name of another user 
+     */
+    private synchronized void sendMessageMergeWall(Socket socket, Wall.Side side, String name) {
+        send(socket, String.format("connect %s %s", side.toString(), name));
+    }
+
+    private synchronized void checkRep() {
+        // only run if assertions are enabled
+        boolean assertionsEnabled = false;
+        assert assertionsEnabled = true; // assignment
+        if (assertionsEnabled) {
+            for (Pair<String, Wall.Side> p: wallLinkMap.keySet()) {
+                String otherBoard = wallLinkMap.get(p);
+                Wall.Side oppositeSide = p.getSecond().opposite();
+                assert wallLinkMap.containsKey(Pair.of(otherBoard, oppositeSide));
+            }
+        }
+    }
+    
+}
