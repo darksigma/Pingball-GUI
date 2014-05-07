a4adfaed93caf0c9212a7daa8f91828fdb572aeb
Prafulla <prafulla@mit.edu>
2014-05-07 18:42:38 -0400

    Working on specs

--- src/main/pingball/gui/BoardGUI.java
+++ src/main/pingball/gui/BoardGUI.java
@@ -1,5 +1,9 @@
 package pingball.gui;
 
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+
 import javax.swing.JPanel;
 
 /**
@@ -12,5 +16,29 @@ import javax.swing.JPanel;
  *
  */
 public class BoardGUI extends JPanel {
+    Color backgroundColor = Color.white;
+    
+    public BoardGUI(){
+        
+    }
+    
+    public void updateFrame(){
+        this.repaint();
+    }
+    
+    @Override
+    public void paintComponent(Graphics g){
+        Graphics2D g2 = (Graphics2D) g;
+        fillWindow(g2);
+        drawBoard(g2);
+    }
 
+    private void drawBoard(Graphics2D g2) {
+        
+    }
+
+    private void fillWindow(Graphics2D g2) {
+        g.setColor(backgroundColor);
+        g.fillRect(0,  0,  getWidth(), getHeight());
+    }
 }
