9425e28ebcd681bb1596a69b5cf20a2a614ee0fc
Natasha Consul <nconsul@mit.edu>
2014-05-07 18:42:33 -0400

    added specs for GUI classes

--- src/main/pingball/gui/BoardGUI.java
+++ src/main/pingball/gui/BoardGUI.java
@@ -2,6 +2,15 @@ package pingball.gui;
 
 import javax.swing.JPanel;
 
+/**
+ * This class is the CONTROLLER of our Model-View-Controller. Here, the board
+ * file is created as a JPanel and is passed to the PingballGUI (View). This
+ * class also contains all the listener methods that update the Model and the
+ * View based on user input.
+ * 
+ * @author nconsul, nkbuduma, prafulla
+ *
+ */
 public class BoardGUI extends JPanel {
 
 }
--- src/main/pingball/gui/PingballGUI.java
+++ src/main/pingball/gui/PingballGUI.java
@@ -3,7 +3,18 @@ package pingball.gui;
 import javax.swing.JFrame;
 import javax.swing.SwingUtilities;
 
-
+/**
+ * This class represents the VIEW of our Model-View-Controller. Here, we implement
+ * changes based on user input (choosing the pingball file, starting, restarting, 
+ * pausing, port/host). This will contain the board that the user can see and play
+ * with. The board is represented as a BoardGUI JPanel.
+ * 
+ * This class will update the GUI at each time frame, based on changes to the
+ * model.
+ * 
+ * @author nconsul, nkbuduma, prafulla
+ *
+ */
 
 public class PingballGUI extends JFrame {
     
