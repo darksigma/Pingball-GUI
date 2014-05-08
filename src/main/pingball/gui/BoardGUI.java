dff4b616791bf0354bdd185ba27da120d42cfb82
Prafulla <prafulla@mit.edu>
2014-05-07 21:19:21 -0400

    Added GUI backbone

--- src/main/pingball/gui/BoardGUI.java
+++ src/main/pingball/gui/BoardGUI.java
@@ -9,6 +9,7 @@ import java.awt.event.KeyEvent;
 
 import javax.swing.JPanel;
 
+import pingball.model.PingballModel;
 import pingball.simulation.Board;
 
 /**
@@ -16,25 +17,29 @@ import pingball.simulation.Board;
  * file is created as a JPanel and is passed to the PingballGUI (View). This
  * class also contains all the listener methods that update the Model and the
  * View based on user input. Whenever there is a key press, we will put the
- * key press message on the Clients's blocking queue (Model).
+ * key press message on the Clients's blocking queue (Model). 
  * 
  * @author nconsul, nkbuduma, prafulla
  *
  */
 public class BoardGUI extends JPanel {
     Color backgroundColor = Color.white;
-    Board board;
+    //Board board;
+    PingballModel pingballModel;
     
-    public BoardGUI(Board board,int width,int height){
-        this.board = board;
+    public BoardGUI(PingballModel pingballModel,int width,int height){
+        this.pingballModel = pingballModel;
         this.setPreferredSize(new Dimension(width, height));
         setFocusable(true);
         requestFocusInWindow();
         addKeyListener(new KeyAdapter() {
+            
+            @Override
             public void keyPressed(KeyEvent e) {
                 //Add the keyPress message to the clients receiveQueue message queue
             }
-
+            
+            @Override
             public void keyReleased(KeyEvent e) {
                 //Add the keyRelease message to the clients receiveQueue message queue
             }
@@ -53,7 +58,7 @@ public class BoardGUI extends JPanel {
     }
 
     private void drawBoard(final Graphics2D g) {
-        
+        //This will draw the current form of all the objects on the board.
     }
 
     private void fillWindow(final Graphics2D g) {
--- src/main/pingball/gui/PingballGUI.java
+++ src/main/pingball/gui/PingballGUI.java
@@ -1,7 +1,14 @@
 package pingball.gui;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+
 import javax.swing.JFrame;
 import javax.swing.SwingUtilities;
+import javax.swing.Timer;
+
+import pingball.model.PingballModel;
 
 /**
  * This class represents the VIEW of our Model-View-Controller. Here, we implement
@@ -10,7 +17,8 @@ import javax.swing.SwingUtilities;
  * with. The board is represented as a BoardGUI JPanel.
  * 
  * This class will update the GUI at each time frame, based on changes to the
- * model. These updates will be made by using a Timer.
+ * model. These updates will be made by using a Timer. Buttons can start, pause or stop the timer.
+ * Timer will always run listener code on Swing event-handling thread.
  * 
  * @author nconsul, nkbuduma, prafulla
  *
@@ -18,16 +26,64 @@ import javax.swing.SwingUtilities;
 
 public class PingballGUI extends JFrame {
     
-    public PingballGUI(){
+    private PingballModel pingballModel;
+    private Timer timer;
+    private ActionListener taskPerformer;
+    private int delay;
+    private int boardWidth;
+    private int boardHeight;
+    private BoardGUI boardGUI;
+    
+    public PingballGUI(String[] args) throws IOException{
        // We add all the labels, text fields,buttons,file chooser etc.
        // We add a BoardGUI object, which is a JPanel
+        pingballModel = new PingballModel(args);
+        
+        boardGUI = new BoardGUI(pingballModel,boardWidth,boardHeight) ;
+        taskPerformer = new ActionListener() {
+           
+            @Override
+            public void actionPerformed(ActionEvent arg0) {
+                pingballModel.evolveFrame();
+                boardGUI.updateFrame();
+            }
+            
+        };
+        timer = new Timer(delay,taskPerformer){
+            /**
+             * 
+             */
+            private static final long serialVersionUID = 1L;
+
+            @Override
+            public void stop(){
+                pingballModel.pause();
+            }
+            
+            @Override
+            public void restart(){
+                pingballModel.restart();
+            }
+            
+            @Override
+            public void start(){
+                pingballModel.start();
+            }
+        };
+        timer.start();
     }
     
-    public static void main(String[] args) {
+    public static void main(final String[] args) {
         // set up the UI (on the event-handling thread)
         SwingUtilities.invokeLater(new Runnable() {
             public void run() {
-                PingballGUI main = new PingballGUI();
+                PingballGUI main = null;
+                try {
+                    main = new PingballGUI(args);
+                } catch (IOException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
                 main.setVisible(true);
             }
         });
